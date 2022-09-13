78089bab012b4865d07d0a8783a2fa9df62a6e91
이준열 <vic2ori@naver.com>
2022-09-13 16:56:04 +0900

    feat: 제플린 연결

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -11,6 +11,14 @@ export default {
   argTypes: {
     backgroundColor: { control: 'color' },
   },
+  parameters: {
+    zeplinLink:
+      'https://app.zeplin.io/project/5e7a6d478204d59183a1c76b/styleguide/components?coid=5eac833c5f1f2f1cb19f4f19',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/dln0rOSU8DMVZzwVcn5txl/Portfolio?node-id=15%3A23',
+    },
+  },
 } as ComponentMeta<typeof Button>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
